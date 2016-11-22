ca58018881de54f8e4da8f4a51a205616242444b
lzkelley <lkelley@cfa.harvard.edu>
2016-11-22 16:50:18 -0500

    ENH: added basic functionality to '__main__.py' --- calls 'sacct' with target format and filters by status.

--- slurpy/__main__.py
+++ slurpy/__main__.py
@@ -1,9 +1,41 @@
 """
 """
 
+import subprocess
+
+KEYS = ['JobID', 'jobname', 'state', 'submit', 'elapsed',
+        'AveVMSize', 'MaxVMSize', 'ReqMem', 'AveDiskRead', 'AveDiskWrite',
+        'partition', 'nodelist']
+
+META_WIDTH = 50
+PRINT_WIDTH = 30
+
 
 def main():
     args = _init_argparse()
+
+    use_keys = [kk + "%{}".format(META_WIDTH) for kk in KEYS]
+    keys = ",".join(use_keys)
+    # print(keys)
+    # return
+
+    command = ['sacct', '--format', keys]
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    text = p.stdout.read()
+    retcode = p.wait()
+    # print("retcode = ", retcode)
+
+    lines = text.split('\n')
+    header = lines.pop(0)
+    lines = lines[1:]
+    for ii, ll in enumerate(lines):
+        # print(ii, ll)
+        parts = ll.split()
+        # nice_parts = "{:4d}: ".format(ii) + ", ".join('{1:{0:}}'.format(PRINT_WIDTH, pp.strip()) for pp in parts)
+        nice_parts = "{:4d}: ".format(ii) + ", ".join('{}'.format(pp.strip()) for pp in parts)
+        if args.status in parts and 'extern' not in parts and 'batch' not in parts:
+            # print(ii, ll)
+            print(nice_parts)
 
     return
 
@@ -24,9 +56,9 @@ def _init_argparse():
     # parser.add_argument("--plot",
     #                     type=int, nargs='*', dest="plot", default=False,
     #                     help="Plot results of analysis and evolution.")
-    # parser.add_argument("-i", "--in",
-    #                     type=str, dest="input", default=None,
-    #                     help="Input directory (look for `evolution` and `settings` objects)")
+    parser.add_argument("--status",
+                        type=str, dest="status", default="RUNNING",
+                        help="Status to filter by (e.g. 'RUNNING', 'COMPLETED')")
     # parser.add_argument("-o", "--out",
     #                     type=str, dest="output", default=None,
     #                     help="Output directory")
