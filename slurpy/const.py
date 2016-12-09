a23eeb6c598f47fd8ec8732bb59aa04b0c4695a3
lzkelley <lkelley@cfa.harvard.edu>
2016-12-09 15:34:58 -0500

    ENH: added 'const.py' for constants, parameters, etc

---
+++ slurpy/const.py
@@ -0,0 +1,9 @@
+"""
+"""
+
+META_WIDTH = 50
+PRINT_WIDTH = 30
+
+SACCT_KEYS = ['JobID', 'jobname', 'state', 'submit', 'elapsed',
+              'AveVMSize', 'MaxVMSize', 'ReqMem', 'AveDiskRead', 'AveDiskWrite',
+              'partition', 'nodelist']
