f03ccb1ded61b7cd119209530eb1574e9fddb3f2
lzkelley <lkelley@cfa.harvard.edu>
2016-12-09 15:35:19 -0500

    MAINT: move 'sacct' command stuff to 'sacct.py'

--- slurpy/__main__.py
+++ slurpy/__main__.py
@@ -1,41 +1,11 @@
 """
 """
-
-import subprocess
-
-KEYS = ['JobID', 'jobname', 'state', 'submit', 'elapsed',
-        'AveVMSize', 'MaxVMSize', 'ReqMem', 'AveDiskRead', 'AveDiskWrite',
-        'partition', 'nodelist']
-
-META_WIDTH = 50
-PRINT_WIDTH = 30
+from slurpy import sacct
 
 
 def main():
     args = _init_argparse()
-
-    use_keys = [kk + "%{}".format(META_WIDTH) for kk in KEYS]
-    keys = ",".join(use_keys)
-    # print(keys)
-    # return
-
-    command = ['sacct', '--format', keys]
-    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    text = p.stdout.read()
-    retcode = p.wait()
-    # print("retcode = ", retcode)
-
-    lines = text.split('\n')
-    header = lines.pop(0)
-    lines = lines[1:]
-    for ii, ll in enumerate(lines):
-        # print(ii, ll)
-        parts = ll.split()
-        # nice_parts = "{:4d}: ".format(ii) + ", ".join('{1:{0:}}'.format(PRINT_WIDTH, pp.strip()) for pp in parts)
-        nice_parts = "{:4d}: ".format(ii) + ", ".join('{}'.format(pp.strip()) for pp in parts)
-        if args.status in parts and 'extern' not in parts and 'batch' not in parts:
-            # print(ii, ll)
-            print(nice_parts)
+    sacct.sacct(args.status)
 
     return
 
--- slurpy/sacct.py
+++ slurpy/sacct.py
@@ -1,2 +1,31 @@
 """Interact with the `sacct` SLURM command.
 """
+import subprocess
+
+from slurpy.const import META_WIDTH, SACCT_KEYS
+
+
+def sacct(status):
+
+    use_keys = [kk + "%{}".format(META_WIDTH) for kk in SACCT_KEYS]
+    keys = ",".join(use_keys)
+
+    command = ['sacct', '--format', keys]
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    text = p.stdout.read()
+    retcode = p.wait()
+    # print("retcode = ", retcode)
+
+    lines = text.split('\n')
+    header = lines.pop(0)
+    lines = lines[1:]
+    for ii, ll in enumerate(lines):
+        # print(ii, ll)
+        parts = ll.split()
+        # nice_parts = "{:4d}: ".format(ii) + ", ".join('{1:{0:}}'.format(PRINT_WIDTH, pp.strip()) for pp in parts)
+        nice_parts = "{:4d}: ".format(ii) + ", ".join('{}'.format(pp.strip()) for pp in parts)
+        if status in parts and 'extern' not in parts and 'batch' not in parts:
+            # print(ii, ll)
+            print(nice_parts)
+
+    return
