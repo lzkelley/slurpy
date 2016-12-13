5ba6e08fed8c9da5c6f0e123285427135eea5c28
lzkelley <lkelley@cfa.harvard.edu>
2016-12-13 12:36:16 -0500

    ENH: Add 'STATUS_KEYS' for acceptable status parameters to 'const.py'

--- slurpy/const.py
+++ slurpy/const.py
@@ -7,3 +7,5 @@ PRINT_WIDTH = 30
 SACCT_KEYS = ['JobID', 'jobname', 'state', 'submit', 'elapsed',
               'AveVMSize', 'MaxVMSize', 'ReqMem', 'AveDiskRead', 'AveDiskWrite',
               'partition', 'nodelist']
+
+STATUS_KEYS = ['RUNNING', 'COMPLETED', 'FAILED']
