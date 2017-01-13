92a7a72bb9b96d93493edcd0d2419849c898d966
lzkelley <lkelley@cfa.harvard.edu>
2017-01-13 17:06:20 -0500

    ENH: in 'utils.print_lines_dicts()' remove diagnostic output, start width of columns based on header values.

--- slurpy/utils.py
+++ slurpy/utils.py
@@ -14,18 +14,19 @@ def print_lines_dicts(lines, header):
 
     num_keys = len(header)
     # Find the maximum length of each component of each line
-    sizes = np.zeros(num_keys, dtype=int)
+    #    Start with the size of the header values
+    sizes = [len(hh) for hh in header]
     for ll in lines:
         for ii, (key, val) in enumerate(ll.items()):
             sizes[ii] = np.maximum(sizes[ii], len(val))
 
     # Create nice formatting string
-    form = SEP_CHAR.join("{{:>{sz}s}}".format(sz=ss) for ss in sizes)
+    form = SEP_CHAR.join("{{: >{sz}s}}".format(sz=ss) for ss in sizes)
     # Print header
     print(form.format(*header))
     # Print each line
     for ll in lines:
-        print(len(ll), num_keys)
+        # print(len(ll), num_keys)
         print(form.format(*ll.values()))
 
     return
