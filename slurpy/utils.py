70de4fac1d7b1325d759b048dd632b63b10c7938
lzkelley <lkelley@cfa.harvard.edu>
2017-01-19 15:57:46 -0500

    ENH: in 'utils.py' get '_filter_fields_in_lines()' working, used in 'print_lines_dicts()'

--- slurpy/utils.py
+++ slurpy/utils.py
@@ -11,17 +11,29 @@ Functions
 
 """
 import numpy as np
+from collections import OrderedDict
 
-from . const import SEP_CHAR
+from . import const
+# from . const import SEP_CHAR
 
 
-def print_lines_dicts(lines, header):
+def print_lines_dicts(_lines, _header, args):
     """Print each line (containing a dict) of `sacct` results.  Format nicely.
     """
     # If there are no selected lines, return
-    if not len(lines):
+    if not len(_lines):
         return
 
+    # Filter out which keys are printed
+    # ---------------------------------
+    # If verbose print all keys (`keys = None`)
+    if args.verbose:
+        keys = None
+    else:
+        keys = const.SACCT_KEYS_PRINT
+    lines, header = _filter_fields_in_lines(_lines, _header, keys=keys)
+
+    # Calculate the proper formatting specification string
     form = _calculate_formatting(lines, header)
 
     # Print header
@@ -35,10 +47,21 @@ def print_lines_dicts(lines, header):
 
 
 def _filter_fields_in_lines(lines, header, keys=None):
+    """Select only the desired `keys` from the given header and each line of lines.
+    """
     if keys is None:
         return lines
 
-    return clean
+    # Find which elements of `header` are desired (i.e. in keys)
+    clean_header = [hh for hh in header if hh in keys]
+    if len(clean_header) == 0:
+        raise ValueError("None of the header values ('{}') are targeted in keys ('{}')".format(
+            header, keys))
+
+    # Create a new list of dict including only the target keys of each line
+    clean_lines = [OrderedDict((hh, ll[hh]) for hh in clean_header)
+                   for ll in lines]
+    return clean_lines, clean_header
 
 
 def _format_line(line, form):
@@ -59,7 +82,7 @@ def _calculate_formatting(lines, header):
             sizes[ii] = np.maximum(sizes[ii], len(val))
 
     # Create nice formatting string
-    form = SEP_CHAR.join("{{: >{sz}s}}".format(sz=ss) for ss in sizes)
+    form = const.SEP_CHAR.join("{{: >{sz}s}}".format(sz=ss) for ss in sizes)
     return form
 
 
